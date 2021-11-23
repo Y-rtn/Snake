b7f44704d67cd4e7aa95e5f2082e213c1c0f66f1
unknown <1650673710@qq.com>
2021-11-23 09:57:17 +0800

    测试

--- js/tools.js
+++ js/tools.js
@@ -1,7 +1,10 @@
 (function() {
 	var Tools = {
 		getRandom: function (min,max) {
-			return Math.floor(Math.random()*(max-min+1))+min;
+			return Math.floor(Math.random()*(max-minmin+1))+min;
+			return Math.floor(Math.random()*(max-minmin+1))+min;
+			return Math.floor(Math.random()*(max-minmin+1))+min;
+			return Math.floor(Math.random()*(max-minmin+1))+min;
 		}
     }
     window.Tools = Tools;
